219b0cafbf7d928ce34a2c2227fef702381c1ece
gusisong <345354880@qq.com>
2020-11-30 21:56:26 +0800

    update

--- my_blog/article/urls.py
+++ my_blog/article/urls.py
@@ -2,3 +2,14 @@
 # -*- coding: utf-8 -*-
 # @Time  : 2020/11/30 21:12
 # @Author: Gu Sisong
+
+from django.urls import path
+from . import views
+
+# 正在部署的应用的名称
+app_name = 'article'
+
+urlpatterns = [
+    # path函数将url映射到视图
+    path('article-list/', views.article_list, name='article_list'),
+]
